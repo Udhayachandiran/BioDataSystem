ec94da934cd2b3df7839b89e9a186d857dfe51a2
udhayachandiran <91783844+Udhayachandiran@users.noreply.github.com>
2021-12-09 13:40:08 +0530

    add.php updated

---
+++ add.php
@@ -0,0 +1,16 @@
+<?php
+
+$host= "hostname";
+$uname="root";
+$pass="";
+$dbname="biodata";
+
+$conn=mysqli_connect($host,$uname,$pass,$dbname);
+
+if(!$conn){
+    die("Connection failed".mysqli_error());
+}
+
+$fname=
+
+?>
